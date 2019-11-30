5a1341a1c0807fc53396b24cc1e6796ece1e16aa
Adrian Kasprzyk <xa.kasprzyk@gmail.com>
2019-11-30 18:00:08 +0100

    URL changes

--- file_download.py
+++ file_download.py
@@ -4,12 +4,12 @@ import requests
 # Download file from url and save as 'result.txt' for later purposes.
 ###
 
-url = "https://raw.githubusercontent.com/FraQu/test_text_analyzer/master/5.txt"
+url = "https://raw.githubusercontent.com/FraQu/test_text_analyzer/test/5.txt"
 download_file = requests.get(url, stream=True)
 
-with open('result.txt', 'wb') as f:
+with open('result.txt', 'wb') as file:
     for chunk in download_file.iter_content(chunk_size=1024):
         if not chunk:
             break
-        f.write(chunk)
+        file.write(chunk)
 
