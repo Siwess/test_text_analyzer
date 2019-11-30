44f373ca0b7fee6930d28bf3455a614a0a4dc26f
Adrian Kasprzyk <xa.kasprzyk@gmail.com>
2019-11-30 19:28:38 +0100

    Shortcut file_url.url to variable "url"

--- file_download.py
+++ file_download.py
@@ -6,8 +6,8 @@ def file_download():
     ###
     # Download file from url and save as 'result.txt' for later purposes.
     ###
-
-    download_file = requests.get(file_url.url, stream=True)
+    url = file_url.url
+    download_file = requests.get(url, stream=True)
 
     with open('result.txt', 'wb') as file:
         for chunk in download_file.iter_content(chunk_size=1024):
