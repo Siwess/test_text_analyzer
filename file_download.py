1293f0e92d44a18e582a8b724d6007e683bdb364
Adrian Kasprzyk <xa.kasprzyk@gmail.com>
2019-11-30 18:58:19 +0100

    Refactor to function

--- file_download.py
+++ file_download.py
@@ -1,15 +1,18 @@
 import requests
 
-###
-# Download file from url and save as 'result.txt' for later purposes.
-###
 
-url = "https://raw.githubusercontent.com/FraQu/test_text_analyzer/test/5.txt"
-download_file = requests.get(url, stream=True)
+def file_download():
+    ###
+    # Download file from url and save as 'result.txt' for later purposes.
+    ###
 
-with open('result.txt', 'wb') as file:
-    for chunk in download_file.iter_content(chunk_size=1024):
-        if not chunk:
-            break
-        file.write(chunk)
+    url = "https://raw.githubusercontent.com/FraQu/test_text_analyzer/test/5.txt"
+    download_file = requests.get(url, stream=True)
+
+    with open('result.txt', 'wb') as file:
+        for chunk in download_file.iter_content(chunk_size=1024):
+            if not chunk:
+                break
+            file.write(chunk)
+
 
