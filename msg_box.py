486468d1549244ceb3228cd30809b3768ea5a1e6
Adrian Kasprzyk <xa.kasprzyk@gmail.com>
2019-12-01 17:18:06 +0100

    Add function description

--- msg_box.py
+++ msg_box.py
@@ -2,4 +2,8 @@ from tkinter import messagebox as msg
 
 
 def _msg_box():
+    ##
+    # Show MSG Box about authors.
+    ##
+
     msg.showinfo("About Text Analyzer", "Created by FraQu, Siwess, mikmike-mj")
