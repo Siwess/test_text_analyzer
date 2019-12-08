dfd227fdfd675dbaab606268c68667adc64cdd42
Adrian Kasprzyk <xa.kasprzyk@gmail.com>
2019-12-08 12:01:00 +0100

    Reorganize code...

--- application.py
+++ application.py
@@ -12,18 +12,24 @@ from msg_box import _msg_box
 class Application:
     def __init__(self):
         self.win = tk.Tk()
+        self.create_widgets()
+        self.text = tk.Text(self.win)
+        self.auto_open_file()
+        self._widgets_styling()
+        self.win.mainloop()
+
+    def _widgets_styling(self):
+        ##
+        # Style widgets
+        ##
         self.win.geometry("500x500")
         self.win.title("Text Analyzer")
         self.win.iconbitmap(default="icons/text_analyzer.ico")
-        self.create_widgets()
-        self.text = tk.Text(self.win)
         self.scrollbar_text = tk.Scrollbar(self.win)
         self.scrollbar_text.place(in_=self.text, relx=1., rely=0, relheight=1.)
         self.scrollbar_text.config(command=self.text.yview)
         self.text.config(yscrollcommand=self.scrollbar_text.set)
         self.text.place(x=0, y=0, relwidth=1, relheight=1, width=- 18)
-        self.auto_open_file()
-        self.win.mainloop()
 
     def _quit(self):
         ##
